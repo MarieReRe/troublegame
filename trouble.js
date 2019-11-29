e47c489068635f2e60f90a7e16af3f5df3d819fc
mrsantons <52573981+mrsantons@users.noreply.github.com>
2019-11-29 09:48:59 -0600

    Add files via upload

---
+++ trouble.js
@@ -0,0 +1,20 @@
+function setup()  {
+    
+    createCanvas( 7000, 7000);
+
+}
+function draw(){
+
+    fill(255, 255, 255);
+    rect(300,300,350, 350); // outer boarder of board game
+
+
+    fill(224, 224, 224);
+    ellipse( 475, 475, 95, 95); //outer dome of dice globe
+
+    fill(192, 192, 192);
+    ellipse( 475, 475, 75, 75); //dice globe
+
+    
+   
+}
